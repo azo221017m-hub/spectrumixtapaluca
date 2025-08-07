a54142602813d21b57984ad0abdd2fb7163cb874
azo221017m-hub <azo221017m@gmail.com>
2025-08-06 20:13:14 -0600

    Subida forzada desde local

--- index.js
+++ index.js
@@ -10,19 +10,6 @@ const db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'));
 
 app.use(express.json());
 
-app.post('/registro', (req, res) => {
-  const { nickname, correo, replica, habilidades } = req.body;
-  const query = `INSERT INTO registros (nickname, correo, replica, habilidades) VALUES (?, ?, ?, ?)`;
-
-  db.run(query, [nickname, correo, replica, habilidades], function (err) {
-    if (err) {
-      console.error('Error al insertar:', err.message);
-      return res.status(500).json({ mensaje: 'Error en el servidor' });
-    }
-
-    res.status(200).json({ mensaje: 'Registro recibido' });
-  });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- public/js/registro.js
+++ public/js/registro.js
@@ -8,6 +8,26 @@ document.getElementById('formRegistro').addEventListener('submit', async functio
     habilidades: document.getElementById('habilidades').value
   };
 
+
+
+
+app.post('/registro', (req, res) => {
+  const { nickname, correo, replica, habilidades } = req.body;
+  const query = `INSERT INTO registros (nickname, correo, replica, habilidades) VALUES (?, ?, ?, ?)`;
+
+  db.run(query, [nickname, correo, replica, habilidades], function (err) {
+    if (err) {
+      console.error('Error al insertar:', err.message);
+      return res.status(500).json({ mensaje: 'Error en el servidor' });
+    }
+
+    res.status(200).json({ mensaje: 'Registro recibido' });
+  });
+});
+
+
+
+
   try {
     const respuesta = await fetch('https://spectrumixtapaluca.onrender.com/registro', {
       method: 'POST',
