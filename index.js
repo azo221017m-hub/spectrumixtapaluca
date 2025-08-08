994b9b7ea189d077a1f274837023aa5944c349c7
azo221017m-hub <azo221017m@gmail.com>
2025-08-07 23:35:24 -0600

    Subida forzada desde local

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
 });
+
+db.all("PRAGMA table_info(jugadores);", (err, rows) => {
+  if (err) {
+    console.error('Error al obtener info tabla jugadores:', err.message);
+  } else {
+    console.log('Estructura tabla jugadores:', rows);
+  }
+});
